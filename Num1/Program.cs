6c7c370f3133e0fca298bfcdefdf1ca5a5f77095
Oiga Serik <Kharitonova.87@mail.ru>
2022-11-11 17:59:21 +0300

    1

---
+++ Num1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+
+int number = 8;
+ void CountNumbers (int number)
+ {
+    if (number <= 1)
+        number.ToString();
+    if(number == 0) return;
+        Console.Write("{0} ", number);
+        CountNumbers(number - 1);
+ }
+ CountNumbers(number);
